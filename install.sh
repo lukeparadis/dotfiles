cabdd0ac79b3c95dae235814ff7de38d525d4438
Ubuntu <ubuntu@ip-10-0-0-99.ec2.internal>
2017-04-13 16:15:36 +0000

    added vim-plug install command

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# install vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 echo "so ~/dev/dotfiles/vim/vimrc.vim" > ~/.vimrc
 
 cp shell/inputrc ~/.inputrc
