08310dffdcc6303809cc5a843c6d7b93044001e0
luke <luke@lukes-MacBook-Pro.local>
2019-12-08 17:48:58 -0500

    updates to install

--- setup-env.sh
+++ setup-env.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # install anaconda first
-mkdir ~/.conf
 
+# install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# install tmix/zsh
 brew install tmux zsh
+
+# install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-eval "$(HOME/anaconda3/bin/conda shell.zsh hook)"
+# set up conda for zsh
+eval "$(HOME/opt/anaconda3/bin/conda shell.zsh hook)"
 conda init zsh
-
-# start up ipython to initialize default profile
-ipython -c ""
-
-# make sure this is created, not a symlink 
-mkdir ~/.shell
 
 # install VimPlug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-mkdir -p ~/.vim/undo
-mkdir -p ~/.vim/
+# install dotfiles
+stow --no-folding input ipython shell tmux vim zsh
