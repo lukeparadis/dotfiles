8bd9b8dc446247528ecd5348c7ee53dfcc54fcff
luke <luke@lukes-MacBook-Pro.local>
2020-07-27 15:47:16 -0400

    removed vim install from setup

--- setup-linux.sh
+++ setup-linux.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+apt install stow zsh
+
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-# install VimPlug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
 # install dotfiles
 stow --no-folding input ipython shell tmux vim zsh screen
+
